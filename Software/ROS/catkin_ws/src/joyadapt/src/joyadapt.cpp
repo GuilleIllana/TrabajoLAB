8a593f2a57160ffa03fbbcc5c072e4f78cba4578
Guillermo Illana Gisbert <47231110+GuilleIllana@users.noreply.github.com>
2022-04-05 19:28:29 +0200

    update

--- Software/ROS/catkin_ws/src/joyadapt/src/joyadapt.cpp
+++ Software/ROS/catkin_ws/src/joyadapt/src/joyadapt.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include <std_msgs/Int32MultiArray.h>
+#include <std_msgs/Float32MultiArray.h>
 
 class joyHandler
 {
@@ -10,24 +11,34 @@ public:
 private:
   void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
   ros::NodeHandle nh_;
-  ros::Publisher joy_pub_;
+  ros::Publisher joy_pub_buttons;
+  ros::Publisher joy_pub_axes;
   ros::Subscriber joy_sub_;
 };
 
 joyHandler::joyHandler() {
   joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &joyHandler::joyCallback, this);
-  joy_pub_ = nh_.advertise<std_msgs::Int32MultiArray>("joyarduino", 9);
+  joy_pub_buttons = nh_.advertise<std_msgs::Int32MultiArray>("joyarduinobu", 9);
+  joy_pub_axes = nh_.advertise<std_msgs::Float32MultiArray>("joyarduinoax", 9);
 }
 
 void joyHandler::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-  std_msgs::Int32MultiArray array;
+  std_msgs::Int32MultiArray array_bu;
+  std_msgs::Float32MultiArray array_ax;
   
-  array.data.clear();
+  array_bu.data.clear();
   for (int i = 0; i < 9; i++){
-  	array.data.push_back(joy->buttons[i]);
+  	array_bu.data.push_back(joy->buttons[i]);
   }
-  joy_pub_.publish(array);
+  
+  array_ax.data.clear();
+  for (int i = 0; i < 6; i++){
+  	array_ax.data.push_back(joy->axes[i]);
+  }
+  
+  joy_pub_buttons.publish(array_bu);
+  joy_pub_axes.publish(array_ax);
 }
 
 
